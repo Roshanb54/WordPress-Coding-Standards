7b225fe8fd1454623ed539c669d58cf67b098523
ernilambar <nilambar@outlook.com>
2017-01-18 14:41:21 +0545

    Add sniff for discouraged iframe

---
+++ WordPress/Tests/Theme/DiscouragedIframeUnitTest.php
@@ -0,0 +1,38 @@
+<?php
+/**
+ * Unit test class for WordPress Coding Standard.
+ *
+ * @package WPCS\WordPressCodingStandards
+ * @link    https://github.com/WordPress-Coding-Standards/WordPress-Coding-Standards
+ * @license https://opensource.org/licenses/MIT MIT
+ */
+
+/**
+ * Unit test class for the DiscouragedIframe sniff.
+ *
+ * @package WPCS\WordPressCodingStandards
+ * @since   0.xx.0
+ */
+class WordPress_Tests_Theme_DiscouragedIframeUnitTest extends AbstractSniffUnitTest {
+
+	/**
+	 * Returns the lines where errors should occur.
+	 *
+	 * @return array <int line number> => <int number of errors>
+	 */
+	public function getErrorList() {
+		return array();
+	}
+
+	/**
+	 * Returns the lines where warnings should occur.
+	 *
+	 * @return array <int line number> => <int number of warnings>
+	 */
+	public function getWarningList() {
+		return array(
+			2 => 1,
+		);
+	}
+
+} // End class.
